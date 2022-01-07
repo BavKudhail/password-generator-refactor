c65e92b5c060541bdc29adf1a2a67d4745999989
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-01-07 14:57:41 +0000

    Created logic for generating random password

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,9 @@
-// 1. Create a set of arrays with user options. 
-// 1.a Lowercase - Uppercase - Numbers - Special Characters 
-// These arrays will be added to our password using .concat
-
 // Password characters
 var charactersUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var charactersLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var charactersNumeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var specialCharacters = ["!", "£", "$", "%", "^", "&", "*", "#", "~"]
 
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -24,14 +19,9 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// 2. In generate password function
-// Create a userPassword variable and set it equal to an empty array = [ " " ]
-
 function generatePassword() {
-  // 1. Determine the length of the password
-  var passwordLength = 0;
-
-  passwordLength = prompt("Please type a number between 8 - 128");
+  // Determine length of password
+  var passwordLength = prompt("Please type a number between 8 - 128");
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Password must be between 8-128 characters long")
@@ -42,29 +32,32 @@ function generatePassword() {
     return generatePassword()
   }
 
-
+  // Determine the characters of the password
   var passwordCharacters = charactersLowercase;
+
   if (confirm("Do you want to include Uppercase?")) {
-    passwordCharacters = passwordCharacters.concat(charactersUppercase)
+    passwordCharacters = passwordCharacters.concat(charactersUppercase);
   } 
   if (confirm("Do you want to include Numbers?")) {
-    passwordCharacters = passwordCharacters.concat(charactersNumeric)
+    passwordCharacters = passwordCharacters.concat(charactersNumeric);
   }
   if (confirm("Do you want to include Symbols?")) {
-    passwordCharacters = passwordCharacters.concat(specialCharacters)
+    passwordCharacters = passwordCharacters.concat(specialCharacters);
   }
-  return passwordCharacters
+  
+  // Generating the password
+  var createPassword = [ ];
+  for (var i = 0; i < passwordLength; i++) {
+  var randomCharacters = Math.floor(Math.random() * passwordCharacters.length);
+  createPassword = createPassword + passwordCharacters[randomCharacters];
+  }
+
+  return createPassword;
+
 }
 
 
 
-
-
-
-// 4. Determine the characters of the password
-// 4.a if confirm(include uppercase?) { userPassword = concat .Lowercase }
-// Repeat above if statements with all conditions
-// 4.b - if password length = 0, alert the user to select a condition
 
 // 5. Generating the password 
 // Userpassword = an empty array
